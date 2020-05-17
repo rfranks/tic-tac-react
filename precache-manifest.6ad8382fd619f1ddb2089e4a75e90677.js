407bb392b4933c1d9d4a2016b0b3b2e4b5411bc3
rfranks <richardfranksjr@hotmail.com>
2020-05-16 20:42:41 -0400

    Updates

---
+++ precache-manifest.6ad8382fd619f1ddb2089e4a75e90677.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd93f2fc7a6c340fd494dc889868dd66",
+    "url": "/tic-tac-react/index.html"
+  },
+  {
+    "revision": "28cb4e961c87e1d6d41a",
+    "url": "/tic-tac-react/static/css/main.04db8b9b.chunk.css"
+  },
+  {
+    "revision": "e639e9352c7739763ed8",
+    "url": "/tic-tac-react/static/js/2.bb6371ca.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tic-tac-react/static/js/2.bb6371ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28cb4e961c87e1d6d41a",
+    "url": "/tic-tac-react/static/js/main.aef1d074.chunk.js"
+  },
+  {
+    "revision": "b81ea45138b1148121d2",
+    "url": "/tic-tac-react/static/js/runtime-main.31b71b45.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
